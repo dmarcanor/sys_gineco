3699845a87343da7d040bbbd2ef55951d2345fb5
gibmyx <gibmyxjobs@gmail.com>
2021-07-24 11:21:00 -0400

    .

--- src/Gineco/Pacientes/Infrastruture/controllers/PacientesViewListControllers.php
+++ src/Gineco/Pacientes/Infrastruture/controllers/PacientesViewListControllers.php
@@ -3,11 +3,10 @@
 
 namespace SysGineco\Gineco\Pacientes\Infrastruture\controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Database\Query\Builder;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 use SysGineco\Gineco\Pacientes\Application\Searcher\PacientesSearcher;
 use SysGineco\Gineco\Pacientes\Application\Searcher\PacientesSearcherRequest;
 
@@ -25,26 +24,8 @@ class PacientesViewListControllers extends Controller
     public function execute(Request $request)
     {
         $response = ($this->searcher)(new PacientesSearcherRequest(
-            $request->all(),
+            $request->all()
         ));
-
-//        $paginate = DB::table('pacientes')
-//            ->where(function (Builder $builder) use ($request) {
-//                if($request->nombre)
-//                    $builder->where('pacientes.nombre', 'like', "%{$request->nombre}%");
-//
-//                if($request->apellido)
-//                    $builder->where('pacientes.apellido', 'like', "%{$request->apellido}%");
-//            })
-//            ->orderBy('created_at', 'desc')
-//            ->select([
-//                'id',
-//                'nombre',
-//                'apellido',
-//                'edad',
-//                'estado_civil',
-//                'created_at',
-//            ])->paginate(10);
 
         return Inertia::render('Pacientes/View/List', [
             'rows' => $response->rows(),
